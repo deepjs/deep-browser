d388415ccd54276213dd096a05730fab4d473d6d
Gilles Coomans <gilles.coomans@gmail.com>
2014-03-13 17:59:37 +0100

    update ie-hacks for IE8

--- sandbox/js/ie-hacks.js
+++ sandbox/js/ie-hacks.js
@@ -3,36 +3,48 @@
 // Add ECMA262-5 method binding if not supported natively
 //
 if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
+    Function.prototype.bind = function(owner) {
+        var that = this;
+        if (arguments.length <= 1) {
             return function() {
                 return that.apply(owner, arguments);
             };
         } else {
-            var args= Array.prototype.slice.call(arguments, 1);
+            var args = Array.prototype.slice.call(arguments, 1);
             return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
+                return that.apply(owner, arguments.length === 0 ? args : args.concat(Array.prototype.slice.call(arguments)));
             };
         }
     };
 }
-Date.now = Date.now || function() { return +new Date(); };
+if (!Date.now)
+    Date.now = Date.now || function() {
+        return +new Date;
+    };
 if (!Object.keys) {
-  Object.keys = function(obj) {
-    var keys = [];
-    for (var i in obj) {
-      if (obj.hasOwnProperty(i)) {
-        keys.push(i);
-      }
-    }
-    return keys;
-  };
+    Object.keys = function(obj) {
+        var keys = [];
+        for (var i in obj) {
+            keys.push(i);
+        }
+        return keys;
+    };
+}
+if (!Object.getOwnPropertyNames) {
+    Object.getOwnPropertyNames = function(obj) {
+        var keys = [];
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                keys.push(i);
+            }
+        }
+        return keys;
+    };
 }
 // Add ECMA262-5 string trim if not supported natively
 //
 if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
+    String.prototype.trim = function() {
         return this.replace(/^\s+/, '').replace(/\s+$/, '');
     };
 }
@@ -40,108 +52,104 @@ if (!('trim' in String.prototype)) {
 // Add ECMA262-5 Array methods if not supported natively
 //
 if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (this[i]===find)
+    Array.prototype.indexOf = function(find, i /*opt*/ ) {
+        if (i === undefined) i = 0;
+        if (i < 0) i += this.length;
+        if (i < 0) i = 0;
+        for (var n = this.length; i < n; i++)
+            if (this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('lastIndexOf' in Array.prototype)) {
-    Array.prototype.lastIndexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= this.length-1;
-        if (i<0) i+= this.length;
-        if (i>this.length-1) i= this.length-1;
-        for (i++; i-->0;) /* i++ because from-argument is sadly inclusive */
-            if (this[i]===find)
+    Array.prototype.lastIndexOf = function(find, i /*opt*/ ) {
+        if (i === undefined) i = this.length - 1;
+        if (i < 0) i += this.length;
+        if (i > this.length - 1) i = this.length - 1;
+        for (i++; i-- > 0;) /* i++ because from-argument is sadly inclusive */
+            if (this[i] === find)
                 return i;
         return -1;
     };
 }
 if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            //if (i in this)
-                action.call(that, this[i], i, this);
+    Array.prototype.forEach = function(action, that /*opt*/ ) {
+        for (var i = 0, n = this.length; i < n; i++)
+        //if (i in this)
+            action.call(that, this[i], i, this);
     };
 }
 if (!('map' in Array.prototype)) {
-    Array.prototype.map= function(mapper, that /*opt*/) {
-        var other= new Array(this.length);
-        for (var i= 0, n= this.length; i<n; i++)
-            //if (i in this)
-                other[i]= mapper.call(that, this[i], i, this);
+    Array.prototype.map = function(mapper, that /*opt*/ ) {
+        var other = new Array(this.length);
+        for (var i = 0, n = this.length; i < n; i++)
+        //if (i in this)
+            other[i] = mapper.call(that, this[i], i, this);
         return other;
     };
 }
 if (!('filter' in Array.prototype)) {
-    Array.prototype.filter= function(filter, that /*opt*/) {
-        var other= [], v;
-        for (var i=0, n= this.length; i<n; i++)
-            if (filter.call(that, v= this[i], i, this))
+    Array.prototype.filter = function(filter, that /*opt*/ ) {
+        var other = [],
+            v;
+        for (var i = 0, n = this.length; i < n; i++)
+            if (filter.call(that, v = this[i], i, this))
                 other.push(v);
         return other;
     };
 }
 if (!('every' in Array.prototype)) {
-    Array.prototype.every= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.every = function(tester, that /*opt*/ ) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (!tester.call(that, this[i], i, this))
                 return false;
         return true;
     };
 }
 if (!('some' in Array.prototype)) {
-    Array.prototype.some= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
+    Array.prototype.some = function(tester, that /*opt*/ ) {
+        for (var i = 0, n = this.length; i < n; i++)
             if (tester.call(that, this[i], i, this))
                 return true;
         return false;
     };
 }
-if (!Array.prototype.reduce)
-{
-  Array.prototype.reduce = function(fun /*, initial*/)
-  {
-    var len = this.length;
-    if (typeof fun != "function")
-      throw new TypeError();
+if (!Array.prototype.reduce) {
+    Array.prototype.reduce = function(fun /*, initial*/ ) {
+        var len = this.length;
+        if (typeof fun !== 'function')
+            throw new TypeError();
 
-    // no value to return if no initial value and an empty array
-    if (len == 0 && arguments.length == 1)
-      throw new TypeError();
+        // no value to return if no initial value and an empty array
+        if (len === 0 && arguments.length == 1)
+            throw new TypeError();
 
-    var i = 0;
-    if (arguments.length >= 2)
-    {
-      var rv = arguments[1];
-    }
-    else
-    {
-      do
-      {
-        //if (i in this)
-        //{
-          rv = this[i++];
-          break;
-        //}
+        var i = 0,
+            rv;
+        if (arguments.length >= 2) {
+            rv = arguments[1];
+        } else {
+            //do
+            //{
+            //if (i in this)
+            //{
+            rv = this[i];
+            // break;
+            //}
 
-        // if array contains no values, no initial value to return
-        if (++i >= len)
-          throw new TypeError();
-      }
-      while (true);
-    }
+            // if array contains no values, no initial value to return
+            //if (++i >= len)
+            //throw new TypeError();
+            //}
+            //while (true);
+        }
 
-    for (; i < len; i++)
-    {
-      //if (i in this)
-        rv = fun.call(null, rv, this[i], i, this);
-    }
+        for (; i < len; i++) {
+            //if (i in this)
+            rv = fun.call(null, rv, this[i], i, this);
+        }
 
-    return rv;
-  };
+        return rv;
+    };
 }
