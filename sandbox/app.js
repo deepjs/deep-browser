6efda460b4bafd5d5c5b96344a9e24278df1ca31
Gilles Coomans <gilles.coomans@gmail.com>
2014-03-26 14:37:03 +0100

    update sandbox verbose:true

--- sandbox/app.js
+++ sandbox/app.js
@@ -8,7 +8,7 @@ define(["require", "deep-browser/index", "deepjs/lib/schema"], function(require,
 		console.log("app-sndbx intialised");
 		$("#run-core-units").click(function(e){
 			e.preventDefault();
-			deep.Unit.run(null, { verbose:false })
+			deep.Unit.run(null, { verbose:true })
 			.done(function(report){
 				console.log("report : ", report);
 				$("#reports-container").html("<pre>"+JSON.stringify(report,null, ' ')+'</pre>');
