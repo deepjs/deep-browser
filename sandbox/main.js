4b748c91324ee667737f206d970815ebc828c270
Gilles Coomans <gilles.coomans@gmail.com>
2014-09-27 02:04:07 +0200

    remove sandbox

--- sandbox/main.js
+++
@@ -1,7 +0,0 @@
-// main.js : load all first dependencies
-require.config({
-	baseUrl: "/libs/"
-});
-require(["deepjs/ie-hacks", "deepjs/json2", "deep-browser/index", "app.js"], function(ieHacks, json2, deep, app ) {
-	app();
-});
